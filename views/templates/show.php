46960d02b0ccdd8f8fc1a8981c7d0ad60e0499d6
Rune Piper <kontakt@runepiper.de>
2019-05-18 00:33:58 +0200

    [TASK] Enrich show view

--- views/templates/show.php
+++ views/templates/show.php
@@ -1,1 +1,16 @@
-<?php echo $recipe->text ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Chef</title>
+    </head>
+    <body>
+        <?php echo $recipe->text ?>
+        Quelle: <a href="<?php echo $recipe->source ?>" target="_blank" rel="noopener"><?php echo $recipe->source ?></a>
+        <ul>
+            <?php foreach($recipe->categories as $category): ?>
+                <li>#<?php echo trim($category) ?></li>
+            <?php endforeach; ?>
+        </ul>
+    </body>
+</html>
