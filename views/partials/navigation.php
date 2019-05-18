210cbd4ae79757a0fe660508cf8febf519b98a29
Malte Riechmann <kontakt@malteriechmann.de>
2019-05-18 12:58:30 +0200

    [TASK] Add all responsible persons

--- views/partials/navigation.php
+++ views/partials/navigation.php
@@ -1,10 +1,10 @@
 <nav class="navigation">
     <ul class="navigation__list">
         <li class="navigation__item">
-            <a href="/" class="link">Start</a>
+            <a href="/" class="link">Alle Rezepte</a>
         </li>
         <li class="navigation__item">
-            <a href="https://www.runepiper.de/" class="link" target="_blank">Impressum</a>
+            Verantwortlich <a href="https://www.runepiper.de/" class="link" target="_blank">Rune Piper</a>, <a href="https://www.malteriechmann.de/" class="link" target="_blank">Malte Riechmann</a>, <a href="https://www.janschill.de/" class="link" target="_blank">Jan Schill</a>
         </li>
     </ul>
 </nav>
