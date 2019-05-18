84ab3b7a9adf159cdeee31eb50a5509b1c37d747
Malte Riechmann <kontakt@malteriechmann.de>
2019-05-18 16:42:27 +0200

    [TASK] Add meta viewport to single view also

--- views/templates/show.php
+++ views/templates/show.php
@@ -2,6 +2,7 @@
 <html class="html">
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="styles.css">
         <title><?php echo $recipe->getTitle() ?> — Chef — allerlei Rezepte</title>
     </head>
