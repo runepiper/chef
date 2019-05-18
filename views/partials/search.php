8155e88424e5c4bcf66d80b2a9c0ed244bdf4fba
Malte Riechmann <kontakt@malteriechmann.de>
2019-05-18 02:23:02 +0200

    [TASK] Remove placeholder from search input

--- views/partials/search.php
+++ views/partials/search.php
@@ -1,5 +1,5 @@
 <form class="form" action="/">
     <input type="hidden" name="action" value="search">
     <label class="form__label form__label--hidden">Suchbegriff</label>
-    <input class="form__input" type="search" name="query" placeholder="Suche…" value="<?php echo $query ?? '' ?>">
+    <input class="form__input" type="search" name="query" value="<?php echo $query ?? '' ?>">
 </form>
