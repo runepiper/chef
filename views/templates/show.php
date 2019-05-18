470f8c22efef49e78a691345b85f9b93531de093
Rune Piper <kontakt@runepiper.de>
2019-05-18 17:27:13 +0200

    [BUGFIX] Add version to styles.scss

--- views/templates/show.php
+++ views/templates/show.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="styles.css">
+        <link rel="stylesheet" type="text/css" href="styles.css?v=<?php echo time() ?>">
         <title><?php echo $recipe->getTitle() ?> — Chef — allerlei Rezepte</title>
     </head>
     <body class="body">
