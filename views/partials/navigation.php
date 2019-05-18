c8564f3bd04bc4a84176e78f3c23f71e81e68521
Malte Riechmann <kontakt@malteriechmann.de>
2019-05-18 14:09:09 +0200

    [TASK] Add missing double dot

--- views/partials/navigation.php
+++ views/partials/navigation.php
@@ -4,7 +4,7 @@
             <a href="/" class="link">Alle Rezepte</a>
         </li>
         <li class="navigation__item">
-            Verantwortlich <a href="https://www.runepiper.de/" class="link" target="_blank">Rune Piper</a>, <a href="https://www.malteriechmann.de/" class="link" target="_blank">Malte Riechmann</a>, <a href="https://www.janschill.de/" class="link" target="_blank">Jan Schill</a>
+            Verantwortlich: <a href="https://www.runepiper.de/" class="link" target="_blank">Rune Piper</a>, <a href="https://www.malteriechmann.de/" class="link" target="_blank">Malte Riechmann</a>, <a href="https://www.janschill.de/" class="link" target="_blank">Jan Schill</a>
         </li>
     </ul>
 </nav>
